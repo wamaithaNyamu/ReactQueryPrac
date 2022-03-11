7ceb85ebd2a937c6604d85f7f50bc232ae01f6d6
Wamaitha <wamaithanyamu@gmail.com>
2022-03-11 15:06:35 +0300

    Fetching data with react query

--- frontend/src/Components/superheroesRQ.js
+++ frontend/src/Components/superheroesRQ.js
@@ -1,3 +1,21 @@
+import {useQuery} from "react-query";
+import axios from "axios";
+
+const fetchSuperheroes =()=>{
+    return axios.get('http://localhost:4000/superheroes')
+}
 export const SuperheroesRQ = () => {
-    return <h2>Super Heroes with react query</h2>
+    // unique key
+   const {isLoading, data} = useQuery('super-heroes',fetchSuperheroes)
+
+    if (isLoading) return <h2>Loading...</h2>
+    return(
+        <>
+        <h2>Super heroes</h2>
+            {data?.data.map(hero => {
+                return <div key={hero.name}>{hero.name}</div>
+            })}
+
+        </>
+    )
 }
