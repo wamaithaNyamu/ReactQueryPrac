11dc71f99558e920af8ba1e7379344cc2271ebb7
Wamaitha <wamaithanyamu@gmail.com>
2022-03-11 15:10:22 +0300

    error handling with react query

--- frontend/src/Components/superheroesRQ.js
+++ frontend/src/Components/superheroesRQ.js
@@ -2,13 +2,14 @@ import {useQuery} from "react-query";
 import axios from "axios";
 
 const fetchSuperheroes =()=>{
-    return axios.get('http://localhost:4000/superheroes')
+    return axios.get('http://localhost:4000/superheroes1')
 }
 export const SuperheroesRQ = () => {
     // unique key
-   const {isLoading, data} = useQuery('super-heroes',fetchSuperheroes)
+   const {isLoading, data,isError, error} = useQuery('super-heroes',fetchSuperheroes)
 
     if (isLoading) return <h2>Loading...</h2>
+    if (isError) return <h2>{error.message}</h2>
     return(
         <>
         <h2>Super heroes</h2>
