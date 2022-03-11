341ec15a2daeb3941bb0f2aa2955733037b1d8ea
Wamaitha <wamaithanyamu@gmail.com>
2022-03-11 16:06:16 +0300

    Callbacks for on error and onsuccess

--- frontend/src/Components/superheroesRQ.js
+++ frontend/src/Components/superheroesRQ.js
@@ -4,6 +4,14 @@ import axios from "axios";
 const fetchSuperheroes =()=>{
     return axios.get('http://localhost:4000/superheroes')
 }
+
+const onSuccess = (data) => {
+   console.log("Success",data)
+}
+
+const onError = (err) => {
+    console.log("Error ",err)
+}
 export const SuperheroesRQ = () => {
     // unique key , function, options - cachetime default is 5min (5 *60*1000)
    const {isLoading, data,isError, error,isFetching,refetch} = useQuery('super-heroes',fetchSuperheroes,
@@ -15,8 +23,10 @@ export const SuperheroesRQ = () => {
            // refetchOnWindowFocus:true,
            // refetchInterval:2000,
            // enabled set to false means the component wont fetch on mount
-           enabled:false
-
+           // enabled:false
+           // onsuccess and on error call backs
+           // onSuccess,
+           // onError
 
 
        })
