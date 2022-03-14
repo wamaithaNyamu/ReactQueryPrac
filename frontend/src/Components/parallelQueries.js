19e3d68f9df26ec134daa454ddec81f47ed922ab
Wamaitha <wamaithanyamu@gmail.com>
2022-03-14 12:03:20 +0300

    Fetching parallel queries

---
+++ frontend/src/Components/parallelQueries.js
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:4000/superheroes')
+}
+
+const fetchFriends = () => {
+    return axios.get('http://localhost:4000/friends')
+}
+
+export const ParallelQueriesPage = () => {
+    //use aliases
+    const { data: superHeroes } = useQuery('super-heroes', fetchSuperHeroes)
+    const { data: friends } = useQuery('friends', fetchFriends)
+    console.log(superHeroes, friends)
+    return <div>Parallel Queries</div>
+}
